114c2c8a79dcc62cfe3e1c751c8cd95ce93a60d1
Chau Nguyen <songchau99@gmail.com>
2020-07-17 12:31:15 -0500

    changed board to 2d array

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,65 +1,50 @@
 class Player
     attr_accessor :marker
-    @@board = [0,1,2,3,4,5,6,
-               7,8,9,10,11,12,13,
-               14,15,16,17,18,19,20,
-               21,22,23,24,25,26,27,
-               'X',29,30,31,32,33,34,
-               'X',36,37,38,39,40,41]
+    @@board = Array.new(7) {Array.new(6)}
 
     def initialize(marker)
         @marker  = marker
     end
 
-    def is_filled_below?(position)
+    def is_filled_below?(x,y)
         #if the position belongs to the last row, return false
-        if [35,36,37,38,39,40,41].include? position
+        if y == 5
             return false
         end
 
-        temp_pos = position + 7
+        #otherwise:
+        temp_y = y + 1
 
-        while ![35,36,37,38,39,40,41].include? temp_pos do
-            if @@board[temp_pos].is_a? Integer
+        while temp_y <= 5 do
+            if @@board[x][temp_y] == nil
                 return false
             end
-            temp_pos += 7
-        end
-
-        #check the last row
-        if @@board[temp_pos].is_a? Integer
-            return false
+            temp_y += 1
         end
 
         return true
     end
 
-    def isInside?(a,b,c,d)  #seems hard to check for a win comb. Consider 2D array
-        empty_board = [0,1,2,3,4,5,6,
-                        7,8,9,10,11,12,13,
-                        14,15,16,17,18,19,20,
-                        21,22,23,24,25,26,27,
-                        'X',29,30,31,32,33,34,
-                        'X',36,37,38,39,40,41]
-
-        if (empty_board.include? a) && (empty_board.include? b) 
-            && (empty_board.include? c) && (empty_board.include? d)
+    def isInside?(x,y)  #seems hard to check for a win comb. Consider 2D array
+        if (x >= 0 && x <= 6 && y >= 0 && y <= 5)
             return true
         end
-        
         return false
     end
 
-    def drop(marker,position)
+    def drop(marker,x,y)
         #a target cell is available to be filled is when all the 
         #cell below it are filled and the target cell is empty
 
-        if (@@board[position].is_a? Integer) && is_filled_below?(position)
-            @@board[position] = marker
+        if (@@board[x][y] == nil) && (is_filled_below?(x,y))
+            @@board[x][y] = marker
             return true
         end
 
         return false
+    end
+
+    def vertical_win?(x,y)
     end
 end
 
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -4,41 +4,59 @@ describe Player do
     describe "#is_filled_below?" do
         it "check last row cell if the \'cells\' below it are filled" do
             user = Player.new('X')
-            expect(user.is_filled_below?(35)). to eq(false)
+            expect(user.is_filled_below?(3,5)). to eq(false)
         end
 
         it "check a cell that have all filled cells below" do
             user = Player.new('X')
-            expect(user.is_filled_below?(21)). to eq(true)
+            board = Player.class_variable_get(:@@board)
+            board[0][3] = 'X'
+            board[0][4] = 'X'
+            board[0][5] = 'X'
+            expect(user.is_filled_below?(0,2)). to eq(true)
         end
 
         it "check a cell that don't have all filled cells below" do
             user = Player.new('X')
-            expect(user.is_filled_below?(29)). to eq(false)
+            expect(user.is_filled_below?(0,1)). to eq(false)
         end
     end
 
     describe "#drop" do
         it "notify a sucessful drop" do
             user = Player.new('X')
-            expect(user.drop('X', 21)).to eq(true)
+            board = Player.class_variable_get(:@@board)
+            board[1][5] = 'X'
+            expect(user.drop('X',1,4)).to eq(true)
         end
 
         it "notify a failed drop" do
             user = Player.new('X')
-            expect(user.drop('X', 29)).to eq(false)
+            expect(user.drop('X', 0,1)).to eq(false)
         end
     end
 
     describe "#isInside?" do
         it "returns true for a win comnbination that is inside the board" do
             user = Player.new('X')
-            expect(user.isInside?(8,9,10,11)).to eq(true)
+            expect(user.isInside?(0,5)).to eq(true)
         end
 
         it "returns false for a win comnbination that is not inside the board" do
             user = Player.new('X')
-            expect(user.isInside?(8,9,10,11)).to eq(true)
+            expect(user.isInside?(0,6)).to eq(false)
+        end
+    end
+
+    describe "vertical_win?" do
+        it "returns true for a win vertiacally" do
+            user = Player.new('X')
+            expect(user.vertical_win?(2,3)).to eq(true)
+        end
+
+        it "returns false for a win vertiacally" do
+            user = Player.new('X')
+            expect(user.vertical_win?(3,3)).to eq(true)
         end
     end
 end
