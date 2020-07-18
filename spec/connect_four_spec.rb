b3044021d4486e6359b340c465ea1420e762013b
Chau Nguyen <43297448+VanQuishi@users.noreply.github.com>
2020-07-18 12:27:17 -0500

    Update connect_four_spec.rb

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -3,6 +3,7 @@ require './lib/connect_four.rb'
 describe Player do
     describe "#is_filled_below?" do
         it "check last row cell if the \'cells\' below it are filled" do
+            Player.empty()
             user = Player.new('X')
             expect(user.is_filled_below?(3,5)). to eq(false)
         end
@@ -48,15 +49,54 @@ describe Player do
         end
     end
 
-    describe "vertical_win?" do
+    describe "#vertical_win?" do
         it "returns true for a win vertiacally" do
             user = Player.new('X')
-            expect(user.vertical_win?(2,3)).to eq(true)
+            board = Player.class_variable_get(:@@board)
+            board[1][2] = 'X'
+            board[1][3] = 'X'
+            board[1][4] = 'X'
+            board[1][5] = 'X'
+            expect(user.vertical_win?(1)).to eq(true)
         end
 
-        it "returns false for a win vertiacally" do
+        it "returns false for no win vertiacally" do
             user = Player.new('X')
-            expect(user.vertical_win?(3,3)).to eq(true)
+            expect(user.vertical_win?(3)).to eq(false)
+        end
+    end
+
+    describe "#horizontal_win?" do
+        it "returns true for a win horizontally" do
+            user = Player.new('X')
+            board = Player.class_variable_get(:@@board)
+            board[0][5] = 'X'
+            board[1][5] = 'X'
+            board[2][5] = 'X'
+            board[3][5] = 'X'
+            expect(user.horizontal_win?(5)).to eq(true)
+        end
+        
+        it "returns false for no win horizontally" do
+            user = Player.new('X')
+            board = Player.class_variable_get(:@@board)
+            expect(user.horizontal_win?(4)).to eq(false)
+        end
+    end
+
+    describe "#diagonal_win?" do
+        it "returns true for a win diagonally" do
+            user = Player.new('X')
+            board = Player.class_variable_get(:@@board)
+            board[2][4] = 'X'
+            board[0][2] = 'X'
+            expect(user.diagonal_win?(3,5)).to eq(true)
+        end
+        
+        it "returns false for no win diagonally" do
+            user = Player.new('X')
+            board = Player.class_variable_get(:@@board)
+            expect(user.diagonal_win?(2,5)).to eq(false)
         end
     end
 end
